37cc555446f0e970bda85ecfa541d5e72d2eb8c3
Iwan Timmer <irtimmer@gmail.com>
2015-06-27 17:29:54 +0200

    Split application in client and library

---
+++ client.h
@@ -0,0 +1,34 @@
+/*
+ * This file is part of Moonlight Embedded.
+ *
+ * Copyright (C) 2015 Iwan Timmer
+ *
+ * Moonlight is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Moonlight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Moonlight; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "xml.h"
+
+#include "limelight-common/Limelight.h"
+
+#include "stdbool.h"
+
+void client_init(const char* serverAddress);
+void client_start_app(STREAM_CONFIGURATION *config, const char* serverAddress, int appId, bool sops, bool localaudio);
+struct app_list* client_applist(const char* serverAddress);
+int client_get_app_id(const char* serverAddress, const char* name);
+void client_pair(const char *address);
+int client_get_server_version(void);
+bool client_is_paired(const char *address);
+int client_get_current_game(const char *address);
+void client_quit_app(const char *address);
